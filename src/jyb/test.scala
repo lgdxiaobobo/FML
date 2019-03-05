27f588533f77a2f29fc9607bd90a7d4ec83e4e4b
jyb <jinxihexi19911007@gmail.com>
2019-03-05 20:22:53 +0800

    add io for test

--- src/jyb/test.scala
+++ src/jyb/test.scala
@@ -4,8 +4,8 @@ import org.apache.spark
 import spark.sql
 import sql.SparkSession
 import sql.DataFrame
-import sql.functions._
 import spark.rdd.RDD
+import java.nio.file.Paths
 
 object test {
 
@@ -13,19 +13,21 @@ object test {
     val cfgDir = args(0)
     val seed0 = args(1).toLong
     val maxIter = args(2).toInt
+    val checkDir = args(3)
     val param = new Param(cfgDir)
     val model = FMModel(param, 20)
-      .setCheckDir("/checkPoint/app_with_sementic/FML")
+      .setCheckDir(checkDir)
       .setUserBlocks(128)
-      .setBatchSize(50000)
+      .setBatchSize(param.getBatchSize)
 
     val ss = SparkSession.builder()
       .appName("FM-Rank")
       .getOrCreate()
     val sc = ss.sparkContext
 
-    val dataDir = "/app_with_sementic/" +
-      "dataset/divide_by_seed42/train"
+    val dataDir = args(4)
+    // "/app_with_sementic/" +
+    //   "dataset/divide_by_seed42/train"
     val data = sc.textFile(dataDir)
       .repartition(1024).map(_.split('|'))
       .map(ps => Usage(ps(1).toInt, ps(2).toInt))
@@ -35,78 +37,70 @@ object test {
     val option2 = data.map(p => (p.i, 1))
       .reduceByKey(_ + _).filter(_._2 >= 10)
     println("[INFO] %d users remains while being used ge 10 users".format(option2.count()))
-    val valid = data.map(p => (p.u, p))
+    val remains = data.map(p => (p.u, p))
       .join(option1).map(_._2._1)
       .map(p => (p.i, p))
       .join(option2).map(_._2._1)
-    println("[INFO] before v.s. after filtering, %d <-> %d".format(data.count(), valid.count()))
+    println("[INFO] before v.s. after filtering, %d <-> %d".format(data.count(), remains.count()))
 
     val rng = new util.Random(seed0)
     for (cv <- 0 until 1) {
       val parts =
-        valid.randomSplit(Array(80, 20), rng.nextLong() ^ cv)
+        remains.randomSplit(Array(80, 20), rng.nextLong() ^ cv)
       val train = parts(0)
-      val test = parts(1)
-      val testDF = getTestDF(ss, train, test)
+      val valid = parts(1)
+      val validDF = getTestDF(ss, train, valid, checkDir)
       val (matW, matH) =
-        model.train(train, testDF, rng.nextLong(), maxIter)
-      val loss = model.eval(testDF, matW, matH)
+        model.train(train, validDF, rng.nextLong(), maxIter)
+      val loss = model.eval(validDF, matW, matH)
       println(s"Final test rank loss $loss")
     }
   }
 
-  def getTestDF(ss: SparkSession,
-                train: RDD[Usage],
-                test: RDD[Usage]):
+  def getTestDF(ss: SparkSession, train: RDD[Usage],
+                valid: RDD[Usage], checkDir: String):
   DataFrame = {
     import ss.implicits._
     val sc = ss.sparkContext
-    val items = train.map(_.i)
-      .distinct().collect()
-    val iBD = sc.broadcast(items)
     val trainDF = train.map(p => (p.u, p.i))
       .aggregateByKey(Array[Int]())(_ :+ _, _ ++ _)
       .toDF("u", "is").as("T1")
       .persist()
-    val testDF = test.map(p => (p.u, p.i))
+    val validDF = valid.map(p => (p.u, p.i))
       .aggregateByKey(Array[Int]())(_ :+ _, _ ++ _)
       .toDF("u", "is").as("T2")
       .persist()
-    val joint = trainDF.join(testDF, trainDF("u") === testDF("u"))
+    val joint = trainDF.join(validDF, trainDF("u") === validDF("u"))
       .select("T1.u", "T1.is", "T2.is")
       .toDF("u", "is", "js")
       .as("J1").persist()
     joint.count()
     trainDF.unpersist()
-    testDF.unpersist()
+    validDF.unpersist()
     val result = joint.as[(Int, Array[Int], Array[Int])]
       .rdd.map{case (u, is, js) =>
         val trainSet = is.toSet
-        val testSet = js.toSet.diff(trainSet)
-        val positive = iBD.value
-          .filter(i => testSet.contains(i))
-        val negative = iBD.value
-          .filter(i => !trainSet.contains(i))
-          .filter(i => !testSet.contains(i))
-      (u, positive, negative)
-    }.toDF("u", "pos", "neg")
-      .as("Test").persist()
+        val validSet =
+          js.filter(j => !trainSet.contains(j))
+      (u, is, validSet)
+    }.toDF("u", "pos0", "pos1")
+      .as("Valid").persist()
     result.count()
     joint.unpersist()
     // check off
-    val testDir =
-      "/checkPoint/app_with_sementic/FML/test"
-    deleteIfExists(sc, testDir)
+    val validDir =
+      Paths.get(checkDir, "valid").toString
+    deleteIfExists(sc, validDir)
     result.as[(Int, Array[Int], Array[Int])].rdd
-      .filter(_._2.nonEmpty)
-      .map{case (u, pos, neg) =>
+      .filter(_._3.nonEmpty)
+      .map{case (u, pos0, pos1) =>
         val uStr = u.toString
-        val posStr = pos.mkString(",")
-        val negStr = neg.mkString(",")
-        s"$uStr|$posStr|$negStr"
+        val pos0Str = pos0.mkString(",")
+        val pos1Str = pos1.mkString(",")
+        s"$uStr|$pos0Str|$pos1Str"
       }.repartition(128)
-      .saveAsTextFile(testDir)
-    sc.textFile(testDir).map(_.split('|'))
+      .saveAsTextFile(validDir)
+    sc.textFile(validDir).map(_.split('|'))
       .map{ps =>
         val u = ps(0).toInt
         val pos =
@@ -114,7 +108,7 @@ object test {
         val neg =
           ps(2).split(',').map(_.toInt)
         (u, pos, neg)
-      }.toDF("u", "pos", "neg")
+      }.toDF("u", "pos0", "pos1")
   }
 
 }
